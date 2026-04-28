7b252d86ef9b6cd5ec6712aaffa8a428eed7cc0b
steve <steve@DESKTOP-UJMPR5D>
2026-04-27 22:00:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using _3dEngine;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+//script wide vars
+Cube cube = null;
+
+
+var settings = new NativeWindowSettings
+{
+    ClientSize = new Vector2i(800, 600),
+    Title = "RetroFPS Test"
+};
+
+using var window = new GameWindow(GameWindowSettings.Default, settings);
+
+window.UpdateFrame += args =>
+{
+    if (window.KeyboardState.IsKeyDown(OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape))
+    {
+        window.Close();
+    }
+};
+
+window.Load += () =>
+{
+    cube = new Cube(new Vector3(0, 0, 0), new Vector3(1, 1, 0));
+};
+
+
+window.RenderFrame += args =>
+{
+
+    GL.ClearColor(0.3f, 0.0f, 0.6f, 1.0f);
+    GL.Clear(ClearBufferMask.ColorBufferBit);
+
+    cube.Draw();
+
+    window.SwapBuffers();
+};
+
+
+window.Run();
