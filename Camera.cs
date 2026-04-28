b1b38aaf62c9b686facd934964824fde71565369
steve <steve@DESKTOP-UJMPR5D>
2026-04-27 22:00:10 -0400

    Adding Camera and true 3D functionalityAdd project files.

---
+++ Camera.cs
@@ -0,0 +1,65 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3dEngine
+{
+    class Camera
+    {
+        // getting world position
+        public Vector3 Position;
+        public Vector3 Front;
+        public Vector3 Up;
+
+        // the movement settings
+        public float Speed;
+        private float yaw = -90f;
+        private float pitch = 0f;
+        private float sensitivity = 0.1f;
+
+        public Matrix4 GetViewMatrix()
+        {
+            return Matrix4.LookAt(Position, Position + Front, Up);
+        }
+
+        public void ProcessInput(KeyboardState keyboard, float deltaTime)
+        {
+            float velocity = Speed * deltaTime;
+
+            if (keyboard.IsKeyDown(Keys.W))
+                Position += Front * velocity;
+            if (keyboard.IsKeyDown(Keys.S))
+                Position -= Front * velocity;
+            if (keyboard.IsKeyDown(Keys.A))
+                Position -= Vector3.Normalize(Vector3.Cross(Front, Up)) * velocity;
+            if (keyboard.IsKeyDown(Keys.D))
+                Position += Vector3.Normalize(Vector3.Cross(Front, Up)) * velocity;
+        }
+
+        public void ProcessMouse(float deltaX, float deltaY)
+        {
+            yaw += deltaX * sensitivity;
+            pitch -= deltaY * sensitivity;
+
+            //clamping the camera
+            pitch = Math.Clamp(pitch, -89f, 89f);
+
+            //finding the x, y, z in that order, ensuring that the length is always 1
+            Front = Vector3.Normalize(new Vector3(
+                MathF.Cos(MathHelper.DegreesToRadians(yaw)) * MathF.Cos(MathHelper.DegreesToRadians(pitch)),
+                MathF.Sin(MathHelper.DegreesToRadians(pitch)),
+                MathF.Sin(MathHelper.DegreesToRadians(yaw)) * MathF.Cos(MathHelper.DegreesToRadians(pitch))
+             ));
+        }
+
+        public Camera(Vector3 startPosition)
+        {
+            Position = startPosition;
+            Front = new Vector3(0, 0, -1);
+            Up = new Vector3(0, 1, 0);
+            Speed = 2.5f;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using _3dEngine;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+//script wide vars
+Cube cube1 = null;
+Cube cube2 = null;
+Camera camera = null;
+
+
+var settings = new NativeWindowSettings
+{
+    ClientSize = new Vector2i(800, 600),
+    Title = "RetroFPS Test"
+};
+
+using var window = new GameWindow(GameWindowSettings.Default, settings);
+
+window.UpdateFrame += args =>
+{
+    if (window.KeyboardState.IsKeyDown(OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape))
+    {
+        window.Close();
+    }
+
+    camera.ProcessInput(window.KeyboardState, (float)args.Time);
+};
+
+window.MouseMove += args =>
+{
+    camera.ProcessMouse(args.DeltaX, args.DeltaY);
+};
+
+window.Load += () =>
+{
+    cube1 = new Cube(new Vector3(-0.6f, 0, 0), new Vector3(1, 0, 0));
+    cube2 = new Cube(new Vector3(0.6f, 0, 0), new Vector3(0, 0, 1));
+
+    camera = new Camera(new Vector3(0, 0, 3));
+
+    // open GL settings
+    window.CursorState = CursorState.Grabbed;
+    GL.Enable(EnableCap.DepthTest);
+};
+
+
+window.RenderFrame += args =>
+{
+
+    GL.ClearColor(0.3f, 0.0f, 0.6f, 1.0f);
+    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+    Matrix4 view = camera.GetViewMatrix();
+    Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(
+        MathHelper.DegreesToRadians(75f), // this is the FOV
+        800f / 600f, //width / height of screen
+        0.1f, // where the camera cuts as in closeness (0 will crash it)
+        100f //render disntance
+    );
+
+    cube1.Draw(view, projection);
+    cube2.Draw(view, projection);
+
+    window.SwapBuffers();
+};
+
+
+window.Run();
